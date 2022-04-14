387c70c973328b6d9b2c33a916c7134e5e4ab4b7
sidikone <aboubakone@gmail.com>
2022-04-14 10:55:58 +0200

    collections commit

---
+++ collections/include/collections.h
@@ -0,0 +1,12 @@
+#ifndef COLLECTIONS_H
+#define COLLECTIONS_H
+
+#include <iostream>
+
+#include "vector_1.h"
+#include "array_1.h"
+#include "list_1.h"
+
+void MainFunctions();
+
+#endif
---
+++ collections/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "collections.h"
+
+using namespace std;
+
+int main(){
+
+    cout << "\n" << endl;
+    // TestFunctions();
+    MainFunctions();
+    cout << "\n" << endl;
+
+
+}
+
